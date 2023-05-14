6098dcc4e7a96602e08685b2e261266d7247e1ea
Adrian Zabolitzki <adrianzab@gmail.com>
2023-05-14 12:56:23 -0600

    add solution for addMenuItem in src/restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -9,10 +9,53 @@ function createRestaurant(restaurantName) {
   }
 }
 
+function addMenuItem(restaurantName, menuItem) {
+  // if (!menuValues[i].includes(menuItem.name)) {
+  console.log(menuItem.type)
+  if (menuItem.type === 'lunch' && !restaurantName.menus.lunch.includes(menuItem)) {
+    restaurantName.menus.lunch.push(menuItem)
+  } else if (menuItem.type === 'breakfast' && !restaurantName.menus.breakfast.includes(menuItem)) {restaurantName.menus.breakfast.push(menuItem)
+  } else if (menuItem.type === 'dinner' && !restaurantName.menus.dinner.includes(menuItem)){restaurantName.menus.dinner.push(menuItem)
+  }
+}
+
+    // console.log('menuItem.name: ', menuItem.name)
+    // console.log('if name includes: ', true);
+    // console.log('menuItems values:', objectValues)
+    // if (objectValues.includes('lunch')) {
+    //       restaurantName.menus.lunch.push(menuItem)
+    //     } else if (objectValues.includes('breakfast')) {
+    //       restaurantName.menus.breakfast.push(menuItem)
+    //     } else {
+    //       restaurantName.menus.dinner.push(menuItem)
+    //     }console.log('whole restaurant object: ', restaurantName)
+    //     }
+  
+
+  // console.log(menuItem)
+  // console.log('HERE --->', menuItem.type)
+  // var objectValues = Object.values(menuItem)
+  // console.log(objectValues)
+  // var menuValues = Object.values(restaurantName.menus)
+  // console.log('menuValues: ', menuValues)
+  
+  // console.log('length:', restaurantName.menus.length)
+  
+  
+
+  
+  
+  // for (var i = 0; i < menuValues.length; i ++) {
+  //   console.log('index number: ', i)
+  //   console.log('menuValues[i]: ', menuValues[i])
+
+    
+
+
 
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
